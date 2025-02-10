841199be36c6f1ac1d4d14cfc07f2f42d2828bde
Franco Palombo <ffpp.2003@hotmail.com>
2025-02-10 15:36:03 -0300

    UI rewrite (#8)
    Big changes on UI. Now, everything is managed by the contents
    of the 'menu' array. Depending on its contents, the different
    menues will show. 'menu' is composed of submenues, on which the
    first element is the title of the submenue, and the subsequent
    elements the different options that will show up when in the
    submenue. All arrays, have to be terminated with a 'NULL' element.

--- main.c
+++ main.c
@@ -45,34 +45,43 @@
 #define BTN_OK GPIO_PIN_6
 #define BTN_GPIO GPIOD
 
+#define CHAR_ARROW_UP 1
+#define CHAR_ARROW_UPNDOWN 2
+#define CHAR_ARROW_DOWN 3
+
 #define TEMP_UPPER_LIMIT 260
 #define TEMP_LOWER_LIMIT 100
 #define STEPPER_UPPER_LIMIT 1750
-#define STEPPER_LOWER_LIMIT 2
+#define STEPPER_LOWER_LIMIT 1
 
 #define EEPROM_BASE 0x4000
-#define EEPROM_TEMP_H 0
-#define EEPROM_TEMP_L 1
-#define EEPROM_SPEED_H 2
-#define EEPROM_SPEED_L 3
+#define EEPROM_TEMP 0
+#define EEPROM_SPEED 2
 
-typedef union REGISTER {
-  uint8_t REG;
-  struct {
-    unsigned : 2;
-    unsigned minus : 1;
-    unsigned ok : 1;
-    unsigned plus : 1;
-    unsigned minusp : 1;
-    unsigned okp : 1;
-    unsigned plusp : 1;
-  }bits;
-}btnReg_t;
+const char back[] = "Volver";
+const char *submenu0_0[] = {"Iniciar", NULL};
+const char *submenu0_1[] = {"Hotend", "Temperatura", back, NULL};
+const char *submenu0_2[] = {"Stepper", "Velocidad", back, NULL};
+const char *submenu1_0[] = {"Detener", NULL};
+const char *submenu1_1[] = {"Informacion", NULL};
+const char *submenu1_3[] = {"Stepper", "Velocidad", "Act/Des Stepper", back, NULL};
 
-const char *menues[] = {"Temperatura", "Velocidad", "Iniciar", "Informacion", "Des/Act Stepper", "Velocidad", "Detener"};
+const char **menu0[] = {submenu0_0, submenu0_1, submenu0_2, NULL}; // First item of EVERY menu is going to work as a "button". No sub-menues there
+const char **menu1[] = {submenu1_0, submenu1_1, submenu0_1, submenu1_3, NULL}; // Repeating submenues is possible by repeating them in their respective menu
+
+const char ***menu[] = {menu0, menu1, NULL};
+
 const uint8_t char_arrUp[] = {0x04, 0x0E, 0x1F, 0x00, 0x00, 0x00, 0x00, 0x00};
 const uint8_t char_arrUpNDown[] = {0x04, 0x0E, 0x1F, 0x00, 0x00, 0x1F, 0x0E, 0x04};
 const uint8_t char_arrDown[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x1F, 0x0E, 0x04};
+
+int8_t button_getEdge(volatile bool newState, volatile bool *currentState, volatile bool *previousState, volatile uint8_t *holdCount);
+size_t count_items(const char **submenu);
+size_t count_submenus(const char ***menu_level);
+bool menu_value_selector(char *buffer, const char *string, const char *unit, const uint16_t lowerLimit, const uint16_t upperLimit, uint16_t *variable, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge);
+void save_to_EEPROM(void *value, uint8_t size, uint8_t address);
+void submenu_hotend(char *buff, uint8_t *currPos, uint8_t *currSubMenu, bool *selected, uint16_t *setTemp, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge, bool running);
+void submenu_stepper(char *buff, uint8_t *currPos, uint8_t *currSubMenu, bool *selected, uint16_t *setSpeed, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge, bool running);
 
 void main(void) { // NOLINT 
   CLK_DeInit();
@@ -93,9 +102,9 @@ void main(void) { // NOLINT
   lcd_gpioinit();
   lcd_init();
   lcd_send_byte(LCD_CTL | LCD_ON | LCD_CUR_OFF | LCD_CUR_NBLNK, LCD_CMD);
-  lcd_create_char(1, char_arrUp);
-  lcd_create_char(2, char_arrUpNDown);
-  lcd_create_char(3, char_arrDown);
+  lcd_create_char(CHAR_ARROW_UP, char_arrUp);
+  lcd_create_char(CHAR_ARROW_UPNDOWN, char_arrUpNDown);
+  lcd_create_char(CHAR_ARROW_DOWN, char_arrDown);
   lcd_setpos(0,0);
   lcd_puts_auto("Bottle Recycler");
   
@@ -111,159 +120,245 @@ void main(void) { // NOLINT
   GPIO_Init(BTN_GPIO, BTN_MINUS | BTN_OK | BTN_PLUS, GPIO_MODE_IN_FL_NO_IT);
 
   _delay_ms(1500);
-  char buff[17];
-  uint8_t menuPos = 0, fastCount = 0;
-  bool selected = 0, minPress = 0, plsPress = 0, okPress = 0, running = 0, stepper = 0;
+  char buff[17], arrow = 0;
+  uint8_t currPos = 0, currMenu = 0, currSubMenu = 0, maxPos = 0, inacTime = 0;
+  bool selected = 0, stepper = 0;
   volatile bool minus = 0, minusp = 0, plus = 0, plusp = 0, ok = 0, okp = 0;
-  uint16_t setTemp = FLASH_ReadByte(EEPROM_BASE | EEPROM_TEMP_H) | FLASH_ReadByte(EEPROM_BASE | EEPROM_TEMP_L);
-  uint16_t setSpeed = FLASH_ReadByte(EEPROM_BASE | EEPROM_SPEED_H) | FLASH_ReadByte(EEPROM_BASE | EEPROM_SPEED_L);
+  volatile int8_t minusEdge = 0,  plusEdge = 0, okEdge = 0;
+  volatile uint8_t minusHold = 0, plusHold = 0, okHold = 0;
+  uint16_t setTemp = FLASH_ReadByte(EEPROM_BASE + EEPROM_TEMP) << 8 | FLASH_ReadByte(EEPROM_BASE + EEPROM_TEMP + 1);
+  uint16_t setSpeed = FLASH_ReadByte(EEPROM_BASE + EEPROM_SPEED) << 8 | FLASH_ReadByte(EEPROM_BASE + EEPROM_SPEED + 1);
   lcd_send_byte(LCD_CLR, LCD_CMD);
   while (1) {
-    minus = GPIO_ReadInputPin(BTN_GPIO, BTN_MINUS);
-    plus = GPIO_ReadInputPin(BTN_GPIO, BTN_PLUS);
-    ok = GPIO_ReadInputPin(BTN_GPIO, BTN_OK);
-    if (minus == 1 && minusp == 0){
-      if (selected) {
-        minPress = 1;
-      } else {
-        if ((menuPos > 0 && !running) || (menuPos > 3 && running))
-          menuPos--;
-      }
-    }
-    if (plus == 1 && plusp == 0) {
-      if (selected){
-        plsPress = 1;
-      } else {
-        if ((menuPos < 3 && !running) || (menuPos < 6 && running))
-        menuPos++;
-      }
-    }
-    if (ok == 1 && okp == 0){
-      if (selected)
-        okPress = 1;
+    minusEdge = button_getEdge(GPIO_ReadInputPin(BTN_GPIO, BTN_MINUS), &minus, &minusp, &minusHold);
+    plusEdge = button_getEdge(GPIO_ReadInputPin(BTN_GPIO, BTN_PLUS), &plus, &plusp, &plusHold);
+    okEdge = button_getEdge(GPIO_ReadInputPin(BTN_GPIO, BTN_OK), &ok, &okp, &okHold);
+
+    if (selected == 0) {
+      if (currMenu == 1)
+        inacTime++;
       else
-        selected = 1;
-    }
+        inacTime = 0;
 
-    if (selected){
-      if (minus == 1 && minusp == 1){
-        fastCount++;
-        if (fastCount >= 10)
-          minPress = 1;
-      }
-      if (minus == 0 && minusp == 1)
-        fastCount = 0;
-      if (plus == 1 && plusp == 1){
-        fastCount++;
-        if (fastCount >= 10)
-          plsPress = 1;
-      }
-      if (plus == 0 && plusp == 1)
-        fastCount = 0;
-      switch (menuPos) {
-        case 0:
-          sprintf(buff, "Temp: %3d%cC    %c", setTemp, 210, (setTemp < TEMP_UPPER_LIMIT && setTemp > TEMP_LOWER_LIMIT) ? 2 : (setTemp == TEMP_UPPER_LIMIT ? 3 : 1));
-          if (minPress) {
-            if (setTemp > TEMP_LOWER_LIMIT)
-              setTemp--;
-            minPress = 0;
+      if (inacTime < 200) {
+        if (minusEdge == 1)
+          if (currPos > 0)
+            currPos--;
+        if (plusEdge == 1)
+          if (currPos < maxPos)
+            currPos++;
+        if (okEdge == 1) {
+          if (currPos == 0 || (currPos == 1 && currMenu == 1)) {
+            // Action occurs (start/stop)
+            selected = 1;
           }
-          if (plsPress) {
-            if (setTemp < TEMP_UPPER_LIMIT)
-              setTemp++;
-            plsPress = 0;
+          if (currSubMenu == 0) {
+            currSubMenu = currPos;
+            currPos = 0;
+          } else {
+            // Action occurs again (set speed/temp/ustep/pid)
+            selected = 1;
           }
-          break;
-        case 5:
-        case 1:
-          sprintf(buff, "Veloc: %-4dRPM %c", setSpeed, (setSpeed < STEPPER_UPPER_LIMIT && setSpeed > STEPPER_LOWER_LIMIT) ? 2 : (setSpeed == STEPPER_UPPER_LIMIT ? 3 : 1));
-          if (minPress) {
-            if (setSpeed > STEPPER_LOWER_LIMIT) {
-              if (setSpeed < 100)
-                setSpeed -= 2;
-              else
-                setSpeed -= 10;
-            }
-            minPress = 0;
-            hr4988_setSpeed(setSpeed);
-          }
-          if (plsPress) {
-            if (setSpeed < STEPPER_UPPER_LIMIT) {
-              if (setSpeed < 100)
-                setSpeed += 2;
-              else
-                setSpeed += 10;
-            }
-            plsPress = 0;
-            hr4988_setSpeed(setSpeed);
-          }
-          break;
-        case 2:
-          running = 1;
-          selected = 0;
-          menuPos = 3;
-          hotend_setTemp(setTemp);
-          hotend_setFanSpeed(50);
-          hr4988_setSpeed(setSpeed);
-          break;
-        case 3:
-          sprintf(buff, "Temp:      %-3d%cC", hotend_getTemp(), 210);
-          break;
-        case 4:
-          stepper = !stepper;
-          selected = 0;
-          hr4988_setStepper((FunctionalState)stepper);
-          break;
-        case 6:
-          running = 0;
-          selected = 0;
-          menuPos = 2;
-          hotend_off();
-          hr4988_setStepper(DISABLE);
-          break;
-      }
-      if (okPress){
-        okPress = 0;
-        if (menuPos == 5 || menuPos == 1){
-          FLASH_Unlock(FLASH_MEMTYPE_DATA);
-          FLASH_ProgramByte(EEPROM_BASE | EEPROM_SPEED_H, setSpeed >> 8);
-          FLASH_ProgramByte(EEPROM_BASE | EEPROM_SPEED_L, setSpeed & 0xFF);
-          FLASH_Lock(FLASH_MEMTYPE_DATA);
         }
-        if (menuPos == 0){
-          FLASH_Unlock(FLASH_MEMTYPE_DATA);
-          FLASH_ProgramByte(EEPROM_BASE | EEPROM_TEMP_H, setTemp >> 8);
-          FLASH_ProgramByte(EEPROM_BASE | EEPROM_TEMP_L, setTemp & 0xFF);
-          FLASH_Lock(FLASH_MEMTYPE_DATA);
+        if (currSubMenu == 0) {
+          maxPos = count_submenus(menu[currMenu]) - 1;
+          if (currPos == 0)
+            arrow = CHAR_ARROW_DOWN;
+          else if (currPos == maxPos)
+            arrow = CHAR_ARROW_UP;
+          else
+            arrow = CHAR_ARROW_UPNDOWN;
+
+          sprintf(buff, "%-15.15s%c", menu[currMenu][currPos][0], arrow);
+        } else {
+          maxPos = count_items(menu[currMenu][currSubMenu]) - 2;
+          if (currPos == 0)
+            arrow = CHAR_ARROW_DOWN;
+          else if (currPos == maxPos)
+            arrow = CHAR_ARROW_UP;
+          else
+            arrow = CHAR_ARROW_UPNDOWN;
+
+          sprintf(buff, "%-15.15s%c", menu[currMenu][currSubMenu][currPos + 1], arrow);
         }
-
-
-        selected = 0;
+      } else {
+        inacTime = 200;
+        sprintf(buff, " %-3.3u\322C  %4.4uRPM ", hotend_getTemp(), setSpeed);
       }
+      if (okEdge == 1 || minusEdge == 1 || plusEdge == 1)
+        inacTime = 0;
     } else {
-      if (running){
-        sprintf(buff, "%-15.15s%c", menues[menuPos], menuPos == 3 ? 3 : (menuPos == 6 ? 1 : 2));
-      } else {
-        sprintf(buff, "%-15.15s%c", menues[menuPos], menuPos == 0 ? 3 : (menuPos == 3 ? 1 : 2));
+      switch (currMenu) {
+        case 0:
+          switch (currSubMenu){
+            case 0:
+              hotend_setTemp(setTemp);
+              hr4988_setSpeed(setSpeed);
+              currMenu = 1;
+              selected = 0;
+              break;
+            case 1:
+              submenu_hotend(buff, &currPos, &currSubMenu, &selected, &setTemp, &minusEdge, &plusEdge, &okEdge, 0);
+              break;
+            case 2:
+              submenu_stepper(buff, &currPos, &currSubMenu, &selected, &setSpeed, &minusEdge, &plusEdge, &okEdge, 0);
+              break;
+          }
+          break;
+        case 1:
+          switch (currSubMenu){
+            case 0:
+              hotend_off();
+              hr4988_setStepper(DISABLE);
+              currMenu = 0;
+              selected = 0;
+              break;
+            case 1:
+              selected = 0;
+              currPos = currSubMenu;
+              currSubMenu = 0;
+              inacTime = 200;
+              break;
+            case 2:
+              submenu_hotend(buff, &currPos, &currSubMenu, &selected, &setTemp, &minusEdge, &plusEdge, &okEdge, 1);
+              break;
+            case 3:
+              submenu_stepper(buff, &currPos, &currSubMenu, &selected, &setSpeed, &minusEdge, &plusEdge, &okEdge, 1);
+              break;
+          }
+
+          break;
       }
     }
-    if (!running){
-      if (hotend_getTemp() > 40)
-        hotend_setFanSpeed(100);
-      else
-        hotend_setFanSpeed(0);
-    }
+
     lcd_setpos(0,0);
     lcd_puts_auto(buff);
-    //buttonStates.REG >>= 3;
-    minusp = minus;
-    plusp = plus;
-    okp = ok;
   }
 }
 
+int8_t button_getEdge(volatile bool newState, volatile bool *currentState, volatile bool *previousState, volatile uint8_t *holdCount) {
+  int8_t edge = 0;
+  *currentState = newState;
+  if (*holdCount < 10){
+    if (*currentState == 1 && *previousState == 0)
+      edge = 1;
+    else if(*currentState == 1 && *previousState == 1)
+      (*holdCount)++;
+    else if(*currentState == 0 && *previousState == 1)
+      edge = -1;
+  } else {
+    if (*holdCount > 48) {
+      edge = 3;
+    } else {
+      edge = 2;
+      (*holdCount)++;
+    }
+    if(*currentState == 0 && *previousState == 1)
+      *holdCount = 0;
+  }
+  *previousState = *currentState;
+  return edge;
+}
 
+size_t count_items(const char **submenu) {
+    size_t count = 0;
+    while (submenu[count] != NULL) {
+        count++;
+    }
+    return count;
+}
 
+size_t count_submenus(const char ***menu_level) {
+    size_t count = 0;
+    while (menu_level[count] != NULL) {
+        count++;
+    }
+    return count;
+}
+
+bool menu_value_selector(char *buffer, const char *string, const char *unit, const uint16_t lowerLimit, const uint16_t upperLimit, uint16_t *variable, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge) {
+  char arrow = 0;
+  if (*minusEdge > 0) {
+    if (*variable > lowerLimit) {
+      if (*minusEdge < 3)
+        (*variable)--;
+      else
+        if ((*variable - 10) > lowerLimit)
+          (*variable) -= 10;
+    }
+  }
+  if (*plusEdge > 0) {
+    if (*variable < upperLimit) {
+      if (*plusEdge < 3)
+        (*variable)++;
+      else
+        if ((*variable + 10) < upperLimit)
+          (*variable) += 10;
+    }
+  }
+  if (*okEdge == 1)
+    return 0;
+  if (*minusEdge == 1 && *plusEdge == 1)
+    *variable = lowerLimit;
+
+  if (*variable == upperLimit)
+    arrow = CHAR_ARROW_DOWN;
+  else if (*variable == lowerLimit)
+    arrow = CHAR_ARROW_UP;
+  else
+    arrow = CHAR_ARROW_UPNDOWN;
+  sprintf(buffer, "%-7.7s %4.4u%-3.3s%c", string, *variable, unit, arrow);
+  return 1;
+}
+
+void save_to_EEPROM(void *value, uint8_t size, uint8_t address) {
+  FLASH_Unlock(FLASH_MEMTYPE_DATA);
+  uint8_t *bytePtr = (uint8_t *) value;
+  for (uint8_t i = 0; i < size; i++)
+    FLASH_ProgramByte(EEPROM_BASE + address + i, bytePtr[i]);
+  FLASH_Lock(FLASH_MEMTYPE_DATA);
+}
+
+void submenu_hotend(char *buff, uint8_t *currPos, uint8_t *currSubMenu, bool *selected, uint16_t *setTemp, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge, bool running) {
+  switch (*currPos + 1) {
+    case 1:
+      *selected = menu_value_selector(buff, "Temp.:", "\322C", TEMP_LOWER_LIMIT, TEMP_UPPER_LIMIT, setTemp, minusEdge, plusEdge, okEdge);
+      break;
+    case 2:
+      //go back
+      *selected = 0;
+      *currPos = *currSubMenu;
+      *currSubMenu = 0;
+      save_to_EEPROM(setTemp, sizeof(*setTemp), EEPROM_TEMP);
+      if (running)
+        hotend_setTemp(*setTemp);
+      break;
+  }
+}
+
+void submenu_stepper(char *buff, uint8_t *currPos, uint8_t *currSubMenu, bool *selected, uint16_t *setSpeed, volatile int8_t *minusEdge, volatile int8_t *plusEdge, volatile int8_t *okEdge, bool running) {
+  switch (*currPos + 1) {
+    case 1:
+      *selected = menu_value_selector(buff, "Veloc.:", "RPM", STEPPER_LOWER_LIMIT, STEPPER_UPPER_LIMIT, setSpeed, minusEdge, plusEdge, okEdge);
+      if (running)
+        hr4988_setSpeed(*setSpeed);
+      break;
+    case 2:
+      if (running) {
+        hr4988_setStepper(ENABLE);
+        *selected = 0;
+        break;
+      }
+      //break;  // <------ CAREFUL ------ this is so i dont need to make two switches depending on the state of 'running' in order to have always the "back" at the end of the list
+    case 3:
+      //go back
+      *selected = 0;
+      *currPos = *currSubMenu;
+      *currSubMenu = 0;
+      save_to_EEPROM(setSpeed, sizeof(*setSpeed), EEPROM_SPEED);
+      break;
+  }
+}
 #ifdef USE_FULL_ASSERT
 
 /**
@@ -279,7 +374,7 @@ void assert_failed(u8* file, u32 line) {
 
   /* Infinite loop */
   uint8_t *tmp;
-  uint8_t buff[16];
+  char buff[16];
   while (1){
     uint8_t eol = 0;
     tmp = file;
@@ -299,7 +394,7 @@ void assert_failed(u8* file, u32 line) {
       _delay_ms(1500);
       tmp++;
     }
-    sprintf(buff, "line: %04d", line);
+    sprintf(buff, "line: %04lu", line);
     lcd_send_byte(LCD_CLR, LCD_CMD);
     lcd_setpos(0,0);
     lcd_puts_auto(buff);
